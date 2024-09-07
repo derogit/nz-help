e1ed81fcdb22e18ddefb2a8e2cd1af7e3ff6ef87
derogit <dentk2010@gmail.com>
2024-09-07 17:21:12 +0300

    bugfix

--- styles.js
+++ styles.js
@@ -17,6 +17,63 @@
   $(".homework-row--header .homework__item:nth-child(3)").text("Дз");
 
   $("head").append(
-    `<style> .dz-container li .rltv .dzc-dz.ecl-text-overflow span{ display: block; } .dz-container li .rltv .dzc-dz.ecl-text-overflow{ max-width: 195px } .scores-table__box{ max-height: unset; overflow: hidden; } .journal-scores-panel{ padding-bottom: 10px } .scores-table .point-table .dropdown .pop_toggle{ display: none; border-width: 0 10px 10px 0; } .pt-point:hover .dropdown .pop_toggle{ display: block; } .homework-table { overflow-x: unset; } .homework-container{ grid-template-columns: 124px 60px 47px minmax(410px, 1fr) 125px minmax(240px, 1fr) 180px 45px; } .journal-choose tr{ grid-template-columns: 0.4fr 1fr; } .journal-choose td a[href^="/journal/"]{ background: #000; padding: 10px 20px } .scores-table tr{ grid-template-rows: 25px; grid-template-columns: 40px 160px repeat(21, minmax(33px, 36px)); } .sidebar-support__link { padding: 5px; } .sidebar-nav__box{ oveflow: hidden; } </style>`
+    `
+    <style>
+    .dz-container li .rltv .dzc-dz.ecl-text-overflow span {
+        display: block;
+    }
+
+    .dz-container li .rltv .dzc-dz.ecl-text-overflow {
+        max-width: 195px
+    }
+
+    .scores-table__box {
+        max-height: unset;
+        overflow: hidden;
+    }
+
+    .journal-scores-panel {
+        padding-bottom: 10px
+    }
+
+    .scores-table .point-table .dropdown .pop_toggle {
+        display: none;
+        border-width: 0 10px 10px 0;
+    }
+
+    .pt-point:hover .dropdown .pop_toggle {
+        display: block;
+    }
+
+    .homework-table {
+        overflow-x: unset;
+    }
+
+    .homework-container {
+        grid-template-columns: 124px 60px 47px minmax(410px, 1fr) 125px minmax(240px, 1fr) 180px 45px;
+    }
+
+    .journal-choose tr {
+        grid-template-columns: 0.4fr 1fr;
+    }
+
+    .journal-choose td a[href^="/journal/"] {
+        background: #000;
+        padding: 10px 20px
+    }
+
+    .scores-table tr {
+        grid-template-rows: 25px;
+        grid-template-columns: 40px 160px repeat(21, minmax(33px, 36px));
+    }
+
+    .sidebar-support__link {
+        padding: 5px;
+    }
+
+    .sidebar-nav__box {
+        overflow: hidden;
+    }
+</style>`
   );
 })();
