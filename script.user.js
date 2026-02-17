47e5b369b435b6803107e5dceada2ff7acdc9560
derogit <36679319+derogit@users.noreply.github.com>
2026-02-17 10:05:23 +0200

    Update version number to 1.3.3.1

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         NZ HELP
 // @namespace    http://tampermonkey.net/
-// @version      1.3.3
+// @version      1.3.3.1
 // @description  Additional functional for NZ
 // @author       Danylo Tkachuk
 // @updateURL    https://raw.githubusercontent.com/derogit/nz-help/main/script.user.js
